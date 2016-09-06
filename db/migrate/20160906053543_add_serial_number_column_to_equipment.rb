809cb7b526b668a1d9913e38bcd7d9124501cc9d
Nicolas Sequera <nicolasequera@gmail.com>
2016-09-06 02:43:41 -0500

    add_index_to_serial_number_of_equipment

--- db/migrate/20160906053543_add_serial_number_column_to_equipment.rb
+++ db/migrate/20160906053543_add_serial_number_column_to_equipment.rb
@@ -2,5 +2,4 @@ class AddSerialNumberColumnToEquipment < ActiveRecord::Migration[5.0]
   def change
     add_column :equipment, :serial_number, :string
   end
-  add_index :equipment, :serial_number, :unique => true
 end

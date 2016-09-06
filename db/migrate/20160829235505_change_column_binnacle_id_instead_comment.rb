5542cf8ea2d7e4eb5a9978560a00ecb15783115f
Nicolas Sequera <nicolasequera@gmail.com>
2016-09-06 02:07:57 -0500

    Change migration

--- db/migrate/20160829235505_change_column_binnacle_id_instead_comment.rb
+++ db/migrate/20160829235505_change_column_binnacle_id_instead_comment.rb
@@ -1,6 +1,5 @@
 class ChangeColumnBinnacleIdInsteadComment < ActiveRecord::Migration[5.0]
   def change
     rename_column :iterations, :comment, :binnacle_id
-    change_column :iterations, :binnacle_id, :integer
   end
 end

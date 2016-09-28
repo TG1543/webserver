dcf490dc602e02e15dfdee9d93147447edb4f7d7
Nicolas Sequera <nicolasequera@gmail.com>
2016-09-27 23:46:06 -0500

    Change equipment controller

--- app/controllers/api/v1/equipments_controller.rb
+++ app/controllers/api/v1/equipments_controller.rb
@@ -9,32 +9,32 @@ class Api::V1::EquipmentsController < ApplicationController
   end
 
   def index
-      respond_with Equipment.all
+    respond_with Equipment.all
   end
 
   def create
     equipment = Equipment.new(equipment_params)
     if equipment.save
-      render json: equipment, status: 201, location: [:api, get_user, equipment]
+      render json: equipment, status: 201, location: [:api, equipment]
     else
       render json: { errors: equipment.errors }, status: 422
     end
   end
 
   def update
-    equipment = Equipment.find(params[:id])
+    equipment = get_equipment
     if equipment.update(project_params)
-      render json: equipment, status: 200, location: [:api, get_user, equipment]
+      render json: equipment, status: 200, location: [:api, equipment]
     else
       render json: { errors: equipment.errors }, status: 422
     end
   end
 
   def toggle_state
-    equipment = Equipment.find(params[:id])
+    equipment = get_equipment
     equipment.active = !equipment.active
     if user.save
-      render json: user, status: 200, location: [:api, get_user, equipment]
+      render json: user, status: 200, location: [:api, equipment]
     else
       render json: { errors: user.errors }, status: 422
     end
@@ -45,10 +45,8 @@ class Api::V1::EquipmentsController < ApplicationController
       params.require(:equipment).permit(:active,:name, :serial_number)
     end
 
-    def get_user
-      user = current_user
-      user = User.find(params[:user_id]) if current_user.is_admin?
-      user
+    def get_equipment
+      Equipment.find(params[:id])
     end
 
 end
